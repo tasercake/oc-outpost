b4014eef5c515a774efddd3c85edb0bbe3f1f44c
Krishna Penukonda <krishna@penukonda.me>
2026-02-16 13:11:58 +0000

    refactor: delete dead files (API server, discovery, removed commands)

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,9 +1,10 @@
 mod commands;
-mod handlers;
+pub mod handlers;
 mod state;
 
 pub use commands::Command;
+pub use handlers::{
+    dispatch_callback, handle_close, handle_help, handle_new, handle_permission_request,
+    handle_projects, handle_session, handle_sessions, handle_status,
+};
 pub use state::BotState;
-
-#[allow(unused_imports)]
-pub use handlers::*;
